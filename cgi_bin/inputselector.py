1fe0f4f385914b89140f492c39324e221874b827
Bobby <rhrosent@buffalo.edu>
2017-09-19 14:31:18 -0400

    Work on table input
    Add and delete entry functions in progress

--- cgi_bin/inputselector.py
+++ cgi_bin/inputselector.py
@@ -1,5 +1,7 @@
 
-import cgi, cgitb, json
+import cgi, cgitb, json, pymysql.cursors
+
+
 
 cgitb.enable()
 
@@ -19,3 +21,49 @@ if type = 'edit':
 if type = 'delete':
 	response = 'Delete request recieved'
 	print(json.JSONEncoder().encode(response))
+	
+
+def SetupDBConnect():
+
+dbconn = pymysql.connect(host='localhost',
+                             user='genericuser',
+                             password='passwd',
+                             db='db',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+							 
+							 
+	
+	
+
+def AddEntry():
+format = cgi.FieldStorage()['format'].getvalue #note or calendar?
+text = cgi.FieldStorage()['text'].getvalue
+user = cgi.FieldStorage()['user'].getvalue
+if(format = note):
+	try:
+		with connection.cursor() as cursor:
+		cmd = "INSERT INTO %s (username, text) VALUES (%s, %s)" #s's represent args
+		cursor.execute(cmd, (format, user, text)); #Args are passed in
+		dbconn.commit(); #change needs to be committed
+if(format = calendar):
+	date = cgi.FieldStorage()['date'].getvalue #TODO Going to have to format this so SQL is ok with it
+	try:
+		with connection.cursor() as cursor:
+		cmd = "INSERT INTO %s (username, text, date) VALUES (%s, %s, %s)" #s's represent args
+		cursor.execute(cmd, (format, user, text, date)); #Args are passed in
+		dbconn.commit(); #change needs to be committed		
+
+
+
+def EditEntry():
+
+def DeleteEntry():
+	format = cgi.FieldStorage()['format'].getvalue #note or calendar?
+	id = cgiFieldStorage()['id'].getvalue		#value is what will be used to figure out what to drop
+	try:
+		with connection.cursor() as cursor:
+		cmd = "DELETE FROM %s WHERE id= %s" #s's represent args
+		cursor.execute(cmd, (format, id)); #Args are passed in
+		dbconn.commit(); #change needs to be committed
+	

b74348658e34de52c5eea68445d33eab38f921e3
BobSevenSevens <rhrosent@buffalo.edu>
2017-10-07 10:20:03 -0400

    db script change
    we're not storing passwords serverside so dont need that col, also username size^ since we're using emails

--- cgi_bin/setupdb.sql
+++ cgi_bin/setupdb.sql
@@ -2,21 +2,19 @@ CREATE DATABASE db;
 USE db;
 
 CREATE TABLE users(
-  username VARCHAR(30),
-  password VARCHAR(3000)
-  
+  username VARCHAR(100)  
 );
 
 CREATE TABLE note(
   id INT NOT NULL AUTO_INCREMENT,
-  username VARCHAR(30),
+  username VARCHAR(100),
   text VARCHAR(100),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE calendar(
   id INT NOT NULL AUTO_INCREMENT,
-  username VARCHAR(30),
+  username VARCHAR(100),
   text VARCHAR(100),
   date DATETIME,
   PRIMARY KEY (id)
